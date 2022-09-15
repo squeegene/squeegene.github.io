c590c54a3c06793ea7504a5f223dd37cfb30ddd3
eugene <ngyeugene@gmail.com>
2022-09-14 23:17:39 -0400

    updated theme and config

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,26 +26,27 @@ PIC = "profile.png"
 EMAIL = "eyn2@cornell.edu"
 PHONE = "860 726 6143"
 WEBSITE = "eugene.ng"
+PORTFOLIO = "portfolio.eugene.ng"
 LINKEDIN = "eugeneyng"
 GITHUB = "eugeneyng"
 PDF = "https://github.com/eugeneyng/eugeneyng.github.io/content/resume.pdf"
 
 CAREER_SUMMARY = "I am a technical project manager with 5+ years experience managing fast-paced military and commercial contracts. On the side, my company, Presque Labs LLC has won or finalized in multiple design challenges (HeroX, YCombinator). In my free time I love to stay active and play volleyball, tennis, and ski."
 
-EDUCATIONS = [
+EDUCATION = [
 	{
 		"degree": "M. Eng, Electrical Engineering",
-		"meta": "University of Connecticut",
+		"institution": "University of Connecticut",
 		"time": "2018 - 2020"
 	},
 	{
 		"degree": "M. Eng, Mechanical Engineering",
-		"meta": "Cornell University",
+		"institution": "Cornell University",
 		"time": "2016 - 2017"
 	},
   {
     "degree": "B.S., Mechanical Engineering",
-    "meta": "Cornell University",
+    "institution": "Cornell University",
     "time": "2012 - 2016"
   }
 ]
@@ -55,21 +56,65 @@ EXPERIENCES = [
 		"job_title": "Manager, Manufacturing Engineering & Operations",
 		"time": "Feb 2017 - Present",
 		"company": "Pratt & Whitney | Raytheon Technologies Corp",
-		"details": "Oversaw procurement of hundreds of capital equipment projects totaling more than $100M per year"	
+		"details": [
+      {
+        "description": "Oversaw procurement of hundreds of capital equipment projects totaling more than $100M per year",
+        "inner_details": [
+          "Wrote technical specifications identifying critical processes and guiding design concepts for external vendors",
+          "Negotiated commercial (pricing, payment structure) and legal terms and conditions to reduce cost and risk",
+          "Validated and led acceptance tests for all projects, including material handling automation equipment, machining and inspection systems, and CNC and welding equipment, etc.",
+          "Provided EAR and ITAR classifications for export of sensitive technical data",
+        ]
+      },
+      {"description": "Converted capital equipment Access database to SQL Server with Agilepoint front-end (2021)"},
+      {"description": "Established new quote approval operations workflow in Sharepoint and decreased process time by 50% (2021)"}
+    ],
 	},
 	{
 		"job_title": "Co-Founder",
 		"time": "Jul 2015 - Present",
 		"company": "Presque Labs LLC",
-		"details": "Maintained and provided site reliability engineering for company website and internal applications, including Docker, Jupyter, Nginx, Nextcloud, and Wordpress"
+		"details": [
+      {
+        "description": "HeroX NextGen Cart Design Challenge winner for airport material transport (AMTC) concept to track and transport secured goods within an airport (2018)",
+        "inner_details": [
+          "Designed electrical circuit in KiCad and prototyped and fabricated PCB through OSH Park",
+          "Developed user interface and linked IoT sensors to SQL database using Python",
+        ]
+      },
+      {
+        "description": "Full stack programming with Flask, Django, and MongoDB for Waddle, an events app (2022)",
+      },
+      {
+        "description": "Generated concepts for Revocube (a 3D puzzle game) and programmed game in Unity with C# (2017)",
+      },
+      {
+        "description": "Built and programmed Context, a text message analytics app, in Android Studio with Java (2016)",
+      },
+      {
+        "description": "Maintained and provided site reliability engineering for company website and internal applications, including Docker, Jupyter, Nginx, Nextcloud, and Wordpress",
+      }
+    ]
 	}
 ]
 
 INTERESTS = [
-	"Piano",
-  "Skiing",
-  "Tennis",
-  "Volleyball"
+  {
+    "item": "Piano",
+    "link": "https://www.youtube.com/channel/UCq9CWmdBPJ167Vo3cEH_Erw"
+  },
+  {
+    "item": "Skiing",
+    "link": "https://www.youtube.com/channel/UCq9CWmdBPJ167Vo3cEH_Erw"
+  },
+  {
+    "item": "Tennis",
+    "link": "https://www.youtube.com/channel/UCq9CWmdBPJ167Vo3cEH_Erw"
+  },
+  {
+    "item": "Volleyball",
+    "link": "https://www.youtube.com/channel/UCq9CWmdBPJ167Vo3cEH_Erw"
+  }
 ]
 
 LANGUAGES = [
@@ -84,19 +129,59 @@ PROJECT_INTRO = ""
 PROJECTS = [
 	{
 		"title": "Nonlinear Optimal Control of the COVID-19 Pandemic in Connecticut (2020)",
-		"tagline": "Created a modified SEIR model of COVID-19 spread in Connecticut, and formulated a discrete, nonlinear, predictive, closed-loop feedback controller to minimize the number of infected. Simulated mathematical model in Python using the GEKKO optimization suite"
+    "details": [
+      {
+        "description": "Created a modified SEIR model of COVID-19 spread in Connecticut, and formulated a discrete, nonlinear, predictive, closed-loop feedback controller to minimize the number of infected."
+      },
+      {
+        "description": "Simulated mathematical model in Python using the GEKKO optimization suite"
+      }
+    ],
+    "link": ""
 	},
 	{
 		"title": "HeroX NextGen Cart Design Challenge | Presque Labs LLC (2018)",
-		"tagline": "Challenge winner for airport material transport (AMTC) concept to track and transport secured goods within an airport. Designed electrical circuit in KiCad and prototyped and fabricated PCB through OSH Park. Developed user interface and linked IoT sensors to SQL database using Python."
+		"details": [
+      {
+        "description": "Challenge winner for airport material transport (AMTC) concept to track and transport secured goods within an airport."
+      },
+      {
+        "description": "Designed electrical circuit in KiCad and prototyped and fabricated PCB through OSH Park."
+      },
+      {
+        "description": "Developed user interface and linked IoT sensors to SQL database using Python."
+      },
+    ],
+    "link": ""
 	},
 	{
 		"title": "Arachnabot, Spider-Inspired Robot | Collective Embodied Intelligence Lab (2016)",
-		"tagline": "Analyzed, modeled, and simulated a to-scale jumping robot with spider-inspired joints in MATLAB, using a differential algebraic equation (DAE) method. Directed micro-fabrication using FDM and SLA 3D printing methods"
+    "details": [
+      {
+        "description": "Analyzed, modeled, and simulated a to-scale jumping robot with spider-inspired joints in MATLAB, using a differential algebraic equation (DAE) method."
+      },
+      {
+        "description": "Directed micro-fabrication using FDM and SLA 3D printing methods"
+      }
+    ]
 	},
   {
 		"title": "Cornell Concrete Canoe (2016)",
-		"tagline": "3rd place finish at 2016 ASCE Concrete Canoe Competition. Coordinated and oversaw 6 subteams (aesthetics, analysis, business, mold, mix, and paddling) and 34 persons for the 2016 ASCE Concrete Canoe Competition. Introduced novel moisture sensors and concrete curing control. Eliminated shrinkage cracking while increasing tensile and compressive strength by 62% and 35%"
+    "details": [
+      {
+        "description": "3rd place finish at 2016 ASCE Concrete Canoe Competition."
+      },
+      {
+        "description": "Coordinated and oversaw 6 subteams (aesthetics, analysis, business, mold, mix, and paddling) and 34 persons for the 2016 ASCE Concrete Canoe Competition."
+      },
+      {
+        "description": "Introduced novel moisture sensors and concrete curing control."
+      },
+      {
+        "description": "Eliminated shrinkage cracking while increasing tensile and compressive strength by 62% and 35%"
+      }
+    ],
+    "link": ""
 	},
 ]
 
